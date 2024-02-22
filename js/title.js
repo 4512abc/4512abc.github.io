7122f71791041c2d11337f65337af307bb8d78b5
Totoro <zwj12345_2020@qq.com>
2024-02-22 13:05:03 +0800

    Site updated: 2024-02-22 13:05:00

--- js/title.js
+++ js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         //离开当前页面时标签显示内容
-        document.title = 'Totoroの小窝';
+        document.title = '何人到此一游';
         clearTimeout(titleTime);
     } else {
         //返回当前页面时标签显示内容
-        document.title = '得意何不尽欢';
+        document.title = 'Totoroの小窝';
         //两秒后变回正常标题
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
